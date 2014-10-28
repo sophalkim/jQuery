10ee2dc73cedac1a54ef9bf52fb53351c27eb794
Sophal Kim <sophal.kim.work@gmail.com>
2014-10-28 09:26:10 -0700

    Practicing

---
+++ scripts/practice2.js
@@ -0,0 +1,29 @@
+var buttonOn = $("#buttonOn");
+buttonOn.on("click", function() {
+	alert("Using the .on() function");
+});
+
+var buttonColor = $("#buttonColor");
+buttonColor.click(function() {
+	if (buttonColor.hasClass("btn-info")) {
+		buttonColor.removeClass("btn-info");
+		buttonColor.addClass("btn-warning");
+	} else {
+		buttonColor.removeClass("btn-warning");
+		buttonColor.addClass("btn-info");
+	}
+});
+
+var allButton = $("#buttonColor2");
+allButton.click(function() {
+	if (allButton.hasClass("btn-primary")) {
+		allButton.removeClass("btn-primary");
+		allButton.addClass("btn-danger");
+	} else {
+		allButton.removeClass("btn-danger");
+		allButton.addClass("btn-primary");
+	}
+});
+
+var thumbnail = $("#waterfall");
+thumbnail.animate({left: "+=200px"}, "slow");
