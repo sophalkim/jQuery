f14fad2d504a8baa278bf496f3fb8d0b48eb7f2c
sophalkim <sophal.kim.work@gmail.com>
2014-12-10 09:08:40 -0800

    Panel heading with Jquery function change

--- scripts/practice2.js
+++ scripts/practice2.js
@@ -104,3 +104,11 @@ lgi1.mouseover(function() {
 lgi1.mouseout(function() {
 	lgi1.text("Red");
 });
+
+var lgi3 = $("#lgi3");
+lgi3.mouseover(function() {
+	lgi3.attr("class", "list-group-item list-group-item-warning");
+});
+lgi3.mouseout(function() {
+	lgi3.attr("class", "list-group-item");
+});
