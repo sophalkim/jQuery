112ab2f1909e3220fac0025359ef946d0b65c203
sophalkim <sophal.kim.work@gmail.com>
2015-01-07 08:55:38 -0800

    Toggle button color when clicked

--- scripts/practice3.js
+++ scripts/practice3.js
@@ -1,4 +1,18 @@
+
+// Shows alert when click
 var buttonOn = $("#buttonOn");
 buttonON.on("click", function() {
 	alert("Using the .on() function");
+});
+
+// Toggle button color when click
+var buttonColor = $("#buttonColor");
+buttonColor.click(function() {
+	if (buttonColor.hasClass("btn-info")) {
+		buttonColor.removeClass("btn-info");
+		buttonColor.addClass("btn-warning");
+	} else {
+		buttonColor.removeClass("btn-warning");
+		buttonColor.addClass("btn-info");
+	}
 });
